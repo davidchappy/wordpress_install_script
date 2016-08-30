c9eb6264041931d17fac38fd181e9918e3862d74
David Chapman <davidchappy@gmail.com>
2016-08-30 15:23:30 -0300

    Initial commit

---
+++ config.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Path to your custom themes/plugins
+PROJECT_PATH="/Users/$USER/Projects"
+
+# Path to your WordPress installs
+SITE_PATH="/Users/$USER/Sites"
+
+# Path to your custom Scripts folder
+SCRIPT_PATH="/Users/$USER/Scripts/wp-install"
+
+# Base URL (sites will be setup as subdirectories)
+BASE_URL="http://localhost/~$USER"
+
+# Database information
+DB_USER="root"
+DB_PASS="root"
+DB_HOST="localhost"
